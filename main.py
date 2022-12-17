7a1c994c78cf33652b14c77bafccbea021fc1f79
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-16 22:24:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ if IMC < 18.5:
 elif IMC >= 18+5 and IMC < 24.9:
     print (f'Seu IMC é de {IMC}, e é classificado como Normal')
 
-elif IMC >= 25 an IMC < 29.9:
+elif IMC >= 25 and IMC < 29.9:
     print (f'Seu IMC é de {IMC}, e é classificado como Sobrepeso')
 
-elif IMC >= 30 an IMC < 39.9:
-    print (f'Seu IMC é de {IMC}, e é classificado como Sobrepeso')
+elif IMC >= 30 and IMC < 39.9:
+    print (f'Seu IMC é de {IMC}, e é classificado como Obeso')
 
 else: 
     print ("Pode parar de comer e começar a malhar pois o negócio está feio! Obesidade Grave")
